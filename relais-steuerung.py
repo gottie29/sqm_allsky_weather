589945e002878de4994fd9ceb379d335b53ea5c0
gottie29 <67120052+gottie29@users.noreply.github.com>
2021-10-23 14:56:15 +0200

    Add files via upload

--- relais-steuerung.py
+++ relais-steuerung.py
@@ -6,18 +6,24 @@ GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 
 # Relay 1
-#GPIO.setup(18, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 # Relay 2
 GPIO.setup(23, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(23, GPIO.HIGH)
+        GPIO.output(18, GPIO.HIGH)
         print('Relay 1 ON')
-        time.sleep(20)
-        GPIO.output(23, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(23, GPIO.HIGH)
+        print('Relay 2 ON')
+        time.sleep(1)
+        GPIO.output(18, GPIO.LOW)
         print('Relay 1 OFF')
-        time.sleep(20)
+        time.sleep(1)
+        GPIO.output(23, GPIO.LOW)
+        print('Relay 2 OFF')
+        time.sleep(1)
         
 finally:
     GPIO.cleanup()
